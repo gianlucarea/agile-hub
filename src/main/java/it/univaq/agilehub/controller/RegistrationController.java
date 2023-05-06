d4a0af38014ec2b6063ae725510c632725fa5346
usama-lb <84199955+usama-lb@users.noreply.github.com>
2023-05-06 15:42:56 +0200

    Registration (Vsta / Controller)

---
+++ src/main/java/it/univaq/agilehub/controller/RegistrationController.java
@@ -0,0 +1,79 @@
+package it.univaq.agilehub.controller;
+
+import it.univaq.agilehub.view.ViewDispatcher;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class RegistrationController extends DataInitializable<User> implements Initializable{
+
+    ViewDispatcher dispatcher = ViewDispatcher.getInstance();
+
+    @FXML
+    private Button avantiButton;
+
+    @FXML
+    private TextField cognome;
+
+    @FXML
+    private DatePicker dataNascita;
+
+    @FXML
+    private Button indietroButton;
+
+    @FXML
+    private TextField nome;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private CheckBox socioBox;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void PasswordAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void avantiAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cognomeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dataNascitaAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void indietroAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nomeActione(ActionEvent event) {
+
+    }
+
+    @FXML
+    void socioAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void usernameAction(ActionEvent event) {
+
+    }
+
+}
