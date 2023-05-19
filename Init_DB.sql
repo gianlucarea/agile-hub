cd646d0770e81814be2488ce7771dd67b470daeb
Gianluca Rea <reagianluca97@gmail.com>
2023-05-19 17:07:13 +0200

    New DB

--- Init_DB.sql
+++ Init_DB.sql
@@ -48,143 +48,70 @@ CREATE TABLE `Booking`  (
   REFERENCES Users(id)
   ON DELETE CASCADE  
   ON UPDATE CASCADE
+  )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `Pitch`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `type` enum('CALCETTO','PALLAVOLO','TENNIS','PADEL','BASKET') ,
+  PRIMARY KEY (`id`)
+  )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
   
-  )ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE  `Time_Slot` (
+`id` int NOT NULL AUTO_INCREMENT,
+`time_slot` varchar(45) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `agile_hub_production`.`Users`
-(
-`name`,
-`surname`,
-`password`,
-`username`,
-`dateOfBirth`,
-`age`,
-`type`)
-VALUES
-(
-'Gianluca',
-'Rea',
-'cGFzc3dvcmQ=',
-'aldino',
-'26/09/1997',
-25,
-'NORMALE');
-
-INSERT INTO `agile_hub_production`.`Users`
-(
-`name`,
-`surname`,
-`password`,
-`username`,
-`dateOfBirth`,
-`age`,
-`type`,
-`sport`)
-VALUES
-(
-'Francesco',
-'Falone',
-'cGFzc3dvcmQx',
-'falone',
-'26/09/1997',
-25,
-'ADMIN',
-'PADEL');
-
-INSERT INTO `agile_hub_production`.`Users`
-(
-`name`,
-`surname`,
-`password`,
-`username`,
-`dateOfBirth`,
-`age`,
-`type`)
-VALUES
-(
-'Mario',
-'Rossi',
-'cGFzc3dvcmQ=',
-'mrossi',
-'26/10/1994',
-28,
-'SOCIO');
+CREATE TABLE `Time_Booking` (
+`id` int NOT NULL AUTO_INCREMENT,
+`pitch_id` INT NOT NULL,
+`booking_id`INT NOT NULL,
+`dateBooking` varchar(45) NOT NULL,
+`time_id` int NOT NULL,
+PRIMARY KEY (`id`, `pitch_id`,`dateBooking`,`time_id`),
+INDEX par_ind (pitch_id),
+CONSTRAINT fk_pitch FOREIGN KEY (pitch_id)
+REFERENCES Pitch(id),
+CONSTRAINT fk_booking_id FOREIGN KEY (booking_id)
+REFERENCES Booking(id),
+CONSTRAINT fk_time_id FOREIGN KEY (time_id)
+REFERENCES Time_Slot(id)
+ON DELETE CASCADE  
+ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-INSERT INTO `agile_hub_production`.`Users`
-(
-`name`,
-`surname`,
-`password`,
-`username`,
-`dateOfBirth`,
-`age`,
-`type`)
-VALUES
-(
-'Luigi',
-'Rossi',
-'cGFzc3dvcmQ=',
-'lrossi',
-'26/10/1994',
-28,
-'SOCIO_PLUS');
 
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Gianluca','Rea','cGFzc3dvcmQ=','aldino','26/09/1997',25,'NORMALE');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Francesco','Falone','cGFzc3dvcmQx','falone','26/09/1997',25,'ADMIN');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Mario','Rossi','cGFzc3dvcmQ=','mrossi','26/10/1994',29,'SOCIO');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Luigi','Rossi','cGFzc3dvcmQ=','lrossi','26/10/1994',29,'SOCIO_PLUS');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Giuseppe','Garibalid','cGFzc3dvcmQ=','garibaldi','26/01/1983',40,'SOCIO_PLUS');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`)VALUES('Giuseppe','Verdi','cGFzc3dvcmQ=','verdi','21/02/1984',39,'SOCIO_PLUS');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`,`sport`)VALUES('Jack','Sparrow','cGFzc3dvcmQ=','jack','20/04/1990',33,'MAESTRO','CALCETTO');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`,`sport`)VALUES('Hector','Barbossa','cGFzc3dvcmQ=','hector','20/02/1993',30,'MAESTRO','PALLAVOLO');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`,`sport`)VALUES('Will','Turner','cGFzc3dvcmQ=','will','10/01/1996',27,'MAESTRO','BASKET');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`,`sport`)VALUES('Elisabeth','Swann','cGFzc3dvcmQ=','elisabeth','12/11/1994',29,'MAESTRO','TENNIS');
+INSERT INTO `agile_hub_production`.`Users`(`name`,`surname`,`password`,`username`,`dateOfBirth`,`age`,`type`,`sport`)VALUES('Joshamee','Gibbs','cGFzc3dvcmQ=','gibbs','07/03/1991',32,'MAESTRO','PADEL');
 
-INSERT INTO `agile_hub_production`.`Users`
-(
-`name`,
-`surname`,
-`password`,
-`username`,
-`dateOfBirth`,
-`age`,
-`type`,
-`sport`)
-VALUES
-(
-'Matteo',
-'Teach',
-'cGFzc3dvcmQ=',
-'teach',
-'22/01/1995',
-28,
-'MAESTRO',
-'TENNIS');
+INSERT INTO `agile_hub_production`.`Teacher_Booking`(`user_id`,`teacher_id`,`dayOfBooking`,`sport`) VALUES (1,7,'30/05/2023','CALCETTO');
+INSERT INTO `agile_hub_production`.`Teacher_Booking`(`user_id`,`teacher_id`,`dayOfBooking`,`sport`) VALUES (3,8,'30/05/2023','PALLAVOLO');
+INSERT INTO `agile_hub_production`.`Teacher_Booking`(`user_id`,`teacher_id`,`dayOfBooking`,`sport`) VALUES (4,9,'30/05/2023','PADEL');
+INSERT INTO `agile_hub_production`.`Teacher_Booking`(`user_id`,`teacher_id`,`dayOfBooking`,`sport`) VALUES (5,10,'30/05/2023','TENNIS');
+INSERT INTO `agile_hub_production`.`Teacher_Booking`(`user_id`,`teacher_id`,`dayOfBooking`,`sport`) VALUES (6,11,'30/05/2023','PADEL');
 
-INSERT INTO `agile_hub_production`.`Teacher_Booking`
-(`user_id`,
-`teacher_id`,
-`dayOfBooking`,
-`sport`)
-VALUES
-(
-1,
-5,
-'30/05/2023',
-'TENNIS');
-
-INSERT INTO `agile_hub_production`.`Teacher_Booking`
-(`user_id`,
-`teacher_id`,
-`dayOfBooking`,
-`sport`)
-VALUES
-(
-4,
-5,
-'25/05/2023',
-'TENNIS');
-
-INSERT INTO `agile_hub_production`.`Booking`
-(
-`user_id`,
-`dateBooking`,
-`numberPlayers`,
-`Sport`)
-VALUES
-(
-4,
-'17/05/2023',
-10,
-'CALCETTO');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('8:00 - 9:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('9:00 - 10:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('10:00 - 11:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('12:00 - 13:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('13:00 - 14:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('14:00 - 15:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('15:00 - 16:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('16:00 - 17:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('17:00 - 18:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('18:00 - 19:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('19:00 - 20:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('20:00 - 21:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('21:00 - 22:00');
+INSERT INTO `agile_hub_production`.`Time_Slot`(`time_slot`) VALUES ('22:00 - 23:00');
