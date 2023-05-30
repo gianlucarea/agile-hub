3b2a91b1da49b1f343de4a94383532c3f11dab61
usama-lb <84199955+usama-lb@users.noreply.github.com>
2023-05-30 15:14:08 +0200

    Testing Bottone inscriviti con le librerie

---
+++ src/test/java/it/univaq/agilehub/model/javaFxTest.java
@@ -0,0 +1,19 @@
+package it.univaq.agilehub.model;
+
+import it.univaq.agilehub.view.ViewDispatcher;
+import it.univaq.agilehub.view.ViewException;
+import org.junit.jupiter.api.Test;
+
+public class javaFxTest {
+    @Test
+    public void testMyMethod()
+    {
+
+        ViewDispatcher dispatcher = ViewDispatcher.getInstance();
+        try {
+            dispatcher.iscrivitiView();
+        } catch (ViewException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
